5f74c18c65ff31a6720fb57ca75511ff219719d2
Isalanya Sam Mwase <isalanyamwasesam@gmail.com>
2024-06-18 19:12:43 +0300

    gitpages debug

--- src/fetchSearch.js
+++ src/fetchSearch.js
@@ -1,7 +1,7 @@
 async function fetchSearch({ queryKey }) {
     const { animal, location, breed } = queryKey[1];
     const res = await fetch(
-      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+      `'https://cors-anywhere.herokuapp.com/http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
     );
   
     if (!res.ok)
