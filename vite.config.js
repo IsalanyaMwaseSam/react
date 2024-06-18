1f8f0981b20b8f7447ba016fa1d17cdf490e529e
Isalanya Sam Mwase <isalanyamwasesam@gmail.com>
2024-06-18 11:43:03 +0300

    git pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from "vite";
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    Plugins: [react()], 
+    plugins: [react()], 
     root: "src",
+    build: {
+        outDir: '../build',
+      },
+    base: 'https://github.com/IsalanyaMwaseSam/React.git/'
     
 })
